d379463765838afda4647ec5bf7ce2d911a8f372
tuanvr98 <nguyenductuan98tb@gmail.com>
2019-03-29 18:32:04 +0700

    add wed api

---
+++ SalesWeb/cancel_order.php
@@ -0,0 +1,7 @@
+<?php
+
+$con=new mysqli("localhost","root","","foodonline");
+$st_check=$con->prepare("delete from temp_order where mobile=?");
+$st_check->bind_param("s", $_GET["mobile"]);
+$st_check->execute();
+
